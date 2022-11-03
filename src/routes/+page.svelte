c011d6865301305290c9375bd2a49ca5bc82ab05
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2022-11-03 22:58:11 +0800

    Base files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="hero is-fullheight">
+  <div class="hero-body is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+    <!-- logo -->
+    <div class="image is-128x128">
+      <img class="is-rounded changeColor" src="https://th.bing.com/th/id/OIP.d3OWT7k9d3TNPLZWNVEYIQHaHa?pid=ImgDet&rs=1" alt="Hain.co logo">
+    </div>
+
+    <!-- title -->
+    <div class="lobster is-size-1 has-text-link">
+      Congratulations!
+    </div>
+
+    <!-- subtitle -->
+    <div class="mb-6">
+      Your email is now verified.
+    </div>
+
+    <div class="button is-rounded is-link mt-6">
+      Download App
+    </div>
+  </div>
+</div>
+
+<style>
+  .changeColor {
+    filter: sepia(100%) hue-rotate(190deg) saturate(500%);
+  }
+</style>
